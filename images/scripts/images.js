5ca0472cc96212624682d6325a6a4ea95b7d6e4f
juan pablo romero <jpablo.romero@gmail.com>
2012-02-24 09:21:34 -0600

    images region paginated in preparation for vertical scrolling

--- images/scripts/images.js
+++ images/scripts/images.js
@@ -1,18 +1,14 @@
 (function($) {
     var TAG = '{IMAGE:{name}}';
-    var self = {
-        init: function() {
-            $('#images-container img').live('click', function(e) {
-                e.preventDefault();
-                var title = $(this).attr('title');
-                var tag = TAG.replace('{name}',title);
-                var textArea = $('#id_source');
-                var text = textArea.val();
-                if(text.indexOf(tag) < 0) {
-                    textArea.val(text + ' ' + tag);
-                }
-            });
+
+    $('#images-container img').live('click', function(e) {
+        e.preventDefault();
+        var title = $(this).attr('title');
+        var tag = TAG.replace('{name}',title);
+        var textArea = $('#id_source');
+        var text = textArea.val();
+        if(text.indexOf(tag) < 0) {
+            textArea.val(text + ' ' + tag);
         }
-    };
-    return self;
-})(jQuery).init();
+    });
+})(jQuery);
--- images/widgets.py
+++ images/widgets.py
@@ -5,10 +5,24 @@ from london.templates import render_to_string
 from models import Image
 
 
+def partition(lst,n):
+    new_lst = [[]]
+    row = new_lst[0]
+    for element in lst:
+        if len(row) == n:
+            row = []
+            new_lst.append(row)
+        row.append(element)
+    return new_lst
+
 class ImagesWidget(forms.Widget):
+
+    image_height = '50px'
+    images_per_row = 7
 
     def render(self, name, value, attrs=None):
         images = Image.query()
-        return render_to_string('image_list.html', {'images':images})
+        lst = partition(images,self.images_per_row)
+        return render_to_string('image_list.html', {'images':lst, 'image_height':self.image_height })
 
 

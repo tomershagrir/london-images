c655b9a5f697712a3649262e1d68401efc096ce9
Bisibuka <bisibuka@gmail.com>
2012-05-07 16:51:28 +0300

    default_context fix

--- images/forms.py
+++ images/forms.py
@@ -6,7 +6,7 @@ class ImageForm(forms.ModelForm):
     class Meta:
         model = Image
 
-    def default_context(self):
+    def default_context(self, *args, **kwargs):
         return {
             'object_verbose_name': self._meta.model._meta.verbose_name,
             'object_verbose_name_plural': self._meta.model._meta.verbose_name_plural

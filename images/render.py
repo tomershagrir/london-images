89c16ad80ec6973f09c96778016c7cb74613dc81
Bisibuka <bisibuka@gmail.com>
2012-05-31 17:37:46 +0300

    fixed small bug with image rendering

--- images/render.py
+++ images/render.py
@@ -9,17 +9,18 @@ class ImagesRender():
         return self._render_images(source)
 
     def _render_images(self, source):
-        regex = re.compile("\{IMAGE:(.*?)\}")
-        list_image = regex.findall(source)
-
-        for name in list_image:
-            try:
-                obj = Image.query().filter(name=name).get()
-                alt = None
-                if obj['alt_text']:
-                    alt = ' alt="%s"' % obj['alt_text']
-                img = '<img src="%s"%s />' % (obj['image'].url, alt)
-                source = source.replace("{IMAGE:%s}" % name, img)
-            except:
-                continue
+        if source is not None:
+            regex = re.compile("\{IMAGE:(.*?)\}")
+            list_image = regex.findall(source)
+    
+            for name in list_image:
+                try:
+                    obj = Image.query().filter(name=name).get()
+                    alt = None
+                    if obj['alt_text']:
+                        alt = ' alt="%s"' % obj['alt_text']
+                    img = '<img src="%s"%s />' % (obj['image'].url, alt)
+                    source = source.replace("{IMAGE:%s}" % name, img)
+                except:
+                    continue
         return source
